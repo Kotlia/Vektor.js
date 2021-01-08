8c9aadcc6bfa19deead37b3141d673b139b11cbc
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-01-08 13:10:00 -0500

    Add cross product

--- src/Vector3.js
+++ src/Vector3.js
@@ -7,4 +7,11 @@ export class Vector3 extends Vector {
     dot(vector3) {
         return super.dot(vector3)
     }
+    cross(vector3) {
+        return new Vector3(
+            this.y * vector3.z - this.z * vector3.y,
+            -(this.x * vector3.z- this.z * vector3.x),
+            this.x * vector3.y - this.y * vector3.x
+        )
+    }
 }
