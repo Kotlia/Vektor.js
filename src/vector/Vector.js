9f7bec69b9e1da8a1a285031b6875a9662b01938
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2021-01-08 17:31:45 -0500

    Add add and subtract

--- src/vector/Vector.js
+++ src/vector/Vector.js
@@ -18,8 +18,30 @@ export class Vector {
         )
     }
     dot(vector) {
-        if (typeof vector === typeof this) { throw new DifferentDimentionError(`Different dimetion! ${this.constructor.name} cannot be dotted with ${vector.constructor.name}`) }
+        if (typeof vector !== typeof this) { throw new DifferentDimentionError(`Different dimetion! ${this.constructor.name} cannot be dotted with ${vector.constructor.name}`) }
         return (this.x * vector.x + this.y * vector.y + (this.z || 0) * (vector.z || 0))
+    }
+    add(vector) {
+        if (typeof vector !== typeof this) { throw new DifferentDimentionError(`Different dimetion! ${this.constructor.name} cannot be added with ${vector.constructor.name}`) }
+        const temp = this
+        temp.setX(this.x + vector.x)
+            .setY(this.y + vector.y)
+        if (this.z) {
+            temp.setZ(this.z + vector.z)
+        }
+        temp.magnitude = this.getMagnitude()
+        return temp
+    }
+    subtract(vector) {
+        if (typeof vector !== typeof this) { throw new DifferentDimentionError(`Different dimetion! ${this.constructor.name} cannot be added with ${vector.constructor.name}`) }
+        const temp = this
+        temp.setX(this.x - vector.x)
+            .setY(this.y - vector.y)
+        if (this.z) {
+            temp.setZ(this.z - vector.z)
+        }
+        temp.magnitude = this.getMagnitude()
+        return temp
     }
     get unit() {
         const temp = this
@@ -32,7 +54,7 @@ export class Vector {
         return temp
     }
     crossMagnitude(vector) {
-        if (typeof vector === typeof this) { throw new DifferentDimentionError(`Different dimetion! ${this.constructor.name} cannot be crossed with ${vector.constructor.name}`) }
+        if (typeof vector !== typeof this) { throw new DifferentDimentionError(`Different dimetion! ${this.constructor.name} cannot be crossed with ${vector.constructor.name}`) }
         return Math.abs(
             this.magnitude * vector.magnitude * Math.sin(this.angleBetween(vector))
         )
